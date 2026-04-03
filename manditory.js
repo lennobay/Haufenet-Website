dbbec5462f339492165c453d8da8bf40a9ba2f5c
lennobay <lennard.e.haufe@outlook.com>
2026-04-03 22:08:05 +0200

    changes in the text and etc.

--- manditory.js
+++ manditory.js
@@ -2,16 +2,16 @@ const paramsString = window.location.search;
 const searchParams = new URLSearchParams(paramsString);
 if (searchParams.get("lg") == "lv") {
     const header = document.querySelector("header");
-    header.innerHTML = `<h2 class="header-sub">Haufenet </h2>
-        <h4 onclick="window.location = '/projects'">Projekti</h 4>`;
+    header.innerHTML = `<h2 onclick = "window.location.href = '/'" class="header-sub">Haufenet </h2>
+        <h4 >Projekti</h 4>`;
 }
 else if (searchParams.get("lg") == "de") {
     const header = document.querySelector("header");
     header.innerHTML = `<h2 class="header-sub">Haufenet </h2>
-        <h4 onclick="window.location = '/projects'">Projekte</h 4>`;
+        <h4 onclick="window.location.href = '/projects'">Projekte</h 4>`;
 }
 else {
     const header = document.querySelector("header");
-    header.innerHTML = `<h2 class="header-sub">Haufenet </h2>
-        <h4 onclick="window.location = '/projects'">Projects</h 4>`;
+    header.innerHTML = `<h2  onclick = "window.location.href = '/'" class="header-sub">Haufenet </h2>
+        <h4  onclick = "window.location.href = '/#projects'">Projects</h 4>`;
 }
